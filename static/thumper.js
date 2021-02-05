405f7a7211354acbdd8cb5ca12315ca90f35c40b
John Xia <john.danger.xia@gmail.com>
2021-02-04 22:27:50 -0500

    Wow a terrible looping pedal.

--- static/thumper.js
+++ static/thumper.js
@@ -3,17 +3,31 @@ function init(stream) {
   let stopButton = document.querySelector('.stop');
 
   let recorder = new MediaRecorder(stream);
+  let chunks = [];
   recordButton.onclick = () => {
     recorder.start();
     console.log(recorder.state);
     recordButton.style.background = "red";
   };
 
+  recorder.ondataavailable = e => chunks.push(e.data);
+
   stopButton.onclick = () => {
     recorder.stop();
-    console.log(recorder.state);
+    console.log(recorder);
     recordButton.style.background = "";
   };
+
+  recorder.onstop = () => {
+    const blob = new Blob(chunks, {"type": "audio/ogg; codecs=opus"});
+    chunks = [];
+    const audioUrl = window.URL.createObjectURL(blob);
+    const audio = new Audio();
+    audio.src = audioUrl;
+    audio.loop = true;
+    audio.play();
+  };
+
 }
 
 navigator.mediaDevices.getUserMedia({audio: true})
