4281bf06bf2fa09d0779e1ec0520ab95a747577d
John Xia <john.danger.xia@gmail.com>
2021-02-05 10:03:37 -0500

    Convert to using AudioBufferSourceNodes, add PCM visualization

--- README.md
+++ README.md
@@ -10,14 +10,15 @@ loopah in yah browsah
 
 ## todo
 
+[x] add visual representation of the audio to the clip display
 [] sync up all looping to the playing of the root clip
   [] replace individual clip play/stop to clip enable/disable
+[] click to set looping start/stop points
 [] when recording a clip, add silence to the beginning to match up timing with root clip
   [] overdub if it goes too long
 [] CSS
 
 [] fix annoying clicking sound at the beginning of recording
-[] add visual representation of the audio to the clip display
 [] allow clips to have names / generate automatically
 
 
--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import ReactDOM from 'react-dom';
 class Thumper extends React.Component {
   constructor(props) {
     super(props);
+    this.audioCtx = new AudioContext();
     this.state = {
       isRecording: false,
       clips: [],
-      chunks: []
+      chunks: [],
     };
   }
 
@@ -17,8 +18,12 @@ class Thumper extends React.Component {
       const stream = await navigator.mediaDevices.getUserMedia({audio: true});
       const recorder = new MediaRecorder(stream);
       recorder.ondataavailable = e => this.state.chunks.push(e.data);
-      recorder.onstop = () => {
-        const clip = <Clip audio={this.makeAudio(this.state.chunks)} />;
+      recorder.onstop = async () => {
+        const audioBuf= await this.makeAudioBuffer(this.state.chunks);
+        const clip = <Clip
+          audioCtx={ this.audioCtx }
+          audioBuf={ audioBuf }
+        />;
         const clips = [...this.state.clips];
         clips.push(clip);
         this.setState({clips, chunks: []})
@@ -27,13 +32,11 @@ class Thumper extends React.Component {
     }
   }
 
-  makeAudio(chunks) {
+  async makeAudioBuffer(chunks) {
     const blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
-    const audioURL = window.URL.createObjectURL(blob);
-    const audio = new Audio();
-    audio.src = audioURL;
-    audio.loop = true;
-    return audio;
+    const arrayBuf = await blob.arrayBuffer();
+    const audioBuf = await this.audioCtx.decodeAudioData(arrayBuf);
+    return audioBuf;
   }
 
   componentWillUnmount() {
@@ -43,13 +46,11 @@ class Thumper extends React.Component {
   }
 
   handleStop() {
-    console.log("stopping recording");
     this.state.recorder.stop();
     this.setState({isRecording: false});
   }
 
   handleStart() {
-    console.log("starting recording");
     this.state.recorder.start();
     this.setState({isRecording: true});
   }
@@ -96,12 +97,59 @@ class ClipList extends React.Component {
 }
 
 class Clip extends React.Component {
+  play() {
+    const {audioCtx, audioBuf} = this.props;
+    const node = audioCtx.createBufferSource();
+    node.buffer = audioBuf;
+    node.connect(audioCtx.destination);
+    node.loop = true;
+    node.start();
+    this.setState({node});
+  }
+
+  stop() {
+    this.state.node.stop();
+  }
+
   render() {
-    console.log(this.props.audio);
     return <div>
-      <button onClick={() => this.props.audio.play()}>play</button>
-      <button onClick={() => this.props.audio.load()}>stop</button>
+      <div>
+        <PCMVis buffer={ this.props.audioBuf } width={ 800 } height={ 200 } />
+      </div>
+      <button onClick={ () => this.play() }>play</button>
+      <button onClick={ () => this.stop() }>stop</button>
     </div>
+  }
+}
+
+class PCMVis extends React.Component {
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.updateCanvas();
+  }
+
+  updateCanvas() {
+    const ctx = this.canvasRef.current.getContext('2d');
+    const data = this.props.buffer.getChannelData(0);
+    const stepSize = Math.floor(this.props.buffer.length / this.props.width);
+    const heightScale = this.props.height/2;
+    for (let x = 0; x < this.props.width; x++) {
+      const index = Math.min(x * stepSize, data.length-1);
+      const amp = data[index];
+      ctx.fillRect(x, heightScale * (1 - amp), 1, heightScale * amp * 2);
+    }
+  }
+
+  render() {
+    return <canvas
+      ref={ this.canvasRef }
+      width={ this.props.width }
+      height={ this.props.height }
+    />
   }
 }
 
